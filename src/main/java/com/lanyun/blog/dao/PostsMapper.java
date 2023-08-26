6975fd0d4d8935011d5dc453af95acb5b0f91480
lanyun <wangkaiyu1103@gmail.com>
2023-08-26 21:36:40 +0800

    fix some bugs

--- src/main/java/com/lanyun/blog/dao/PostsMapper.java
+++ src/main/java/com/lanyun/blog/dao/PostsMapper.java
@@ -2,6 +2,7 @@ package com.lanyun.blog.dao;
 
 import com.lanyun.blog.model.Posts;
 import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import org.apache.ibatis.annotations.Mapper;
 
 /**
 * @author lanyun
@@ -9,6 +10,7 @@ import com.baomidou.mybatisplus.core.mapper.BaseMapper;
 * @createDate 2023-08-26 01:14:04
 * @Entity com.lanyun.blog.model.Posts
 */
+@Mapper
 public interface PostsMapper extends BaseMapper<Posts> {
 
 }
--- src/main/java/com/lanyun/blog/model/Posts.java
+++ src/main/java/com/lanyun/blog/model/Posts.java
@@ -1,5 +1,7 @@
 package com.lanyun.blog.model;
 
+import lombok.Data;
+
 import java.io.Serializable;
 import java.util.Date;
 
@@ -7,6 +9,7 @@ import java.util.Date;
  * 
  * @TableName posts
  */
+@Data
 public class Posts {
     /**
      *
